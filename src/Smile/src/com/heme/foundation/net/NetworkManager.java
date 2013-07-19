a93844a984f1f1cbca8ae06e01c0979ba3fc93b3
ottozheng <zhengzhe@zhengzhetekiMacBook-Air.local>
2013-07-20 03:50:47 +0800

    网络

--- src/Smile/src/com/heme/foundation/net/NetworkManager.java
+++ src/Smile/src/com/heme/foundation/net/NetworkManager.java
@@ -1,6 +1,67 @@
 package com.heme.foundation.net;
 
-public class NetworkManager {
+import java.io.IOException;
 
 import android.os.Looper;
+
+import com.ning.http.client.ListenableFuture;
+
+public class NetworkManager implements INetworkManagerInterface
+{
+	protected static NetworkManager gNetworkManager = null;
+	
+	public static INetworkManagerInterface shareManager()
+	{
+		if (gNetworkManager == null) 
+		{
+			gNetworkManager = new NetworkManager();
+		}
+		
+		return gNetworkManager;
+	}
+
+	@Override
+	public Boolean sentNetworkRequest(NetworkRequest request) 
+	{
+		try
+        {
+			ListenableFuture<NetworkResponse> returnVal = null;
+			request.mCallerLooper = Looper.myLooper();
+			returnVal = request.makeURLConnectionConnection(null).prepareRequest(request.mRequest).execute(request.mHandler);
+			if (returnVal == null)
+            {
+	            request.mHandler.onTimeOut(request);
+            }
+        }
+        catch (IOException e)
+        {
+        	request.mHandler.onTimeOut(request);
+        	e.printStackTrace();
+
+        	return false;
+        }
+		catch (Exception e) {
+			request.mHandler.onThrowable(e);
+        	e.printStackTrace();
+
+			return false;
+		}
+		return true;
+    
+	}
+
+	@Override
+	public void cancelNetworkRequest(NetworkRequest request) {
+	    try
+        {
+	    	if (request.mConnectionClient != null && !request.mConnectionClient.isClosed())
+            {
+	    		request.mConnectionClient.close();
+            }
+        }
+        catch (Exception e)
+        {
+        	e.printStackTrace();
+        }		
+	}
 }
