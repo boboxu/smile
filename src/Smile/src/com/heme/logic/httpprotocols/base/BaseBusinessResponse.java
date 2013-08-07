3074aaf5bdbb49ef751e7ce11c1ce03748621914
bobo__x <bobo__x@126.com>
2013-08-08 00:09:35 +0800

    更新登陆回包

--- src/Smile/src/com/heme/logic/httpprotocols/base/BaseBusinessResponse.java
+++ src/Smile/src/com/heme/logic/httpprotocols/base/BaseBusinessResponse.java
@@ -1,8 +1,15 @@
 package com.heme.logic.httpprotocols.base;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.heme.commonlogic.servermanager.BasePbResponse;
+import com.heme.logic.module.Data.DataSvrProto;
 
 
 public class BaseBusinessResponse extends BasePbResponse{
-	
+	protected DataSvrProto mDataSvrProto = null;
+	@Override
+	public void parseData() throws InvalidProtocolBufferException {
+		super.parseData();
+		mDataSvrProto = DataSvrProto.parseFrom(mTransData.getBytesBody());
+	}
 }
